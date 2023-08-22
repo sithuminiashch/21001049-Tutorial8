a3a6b5f4e01761d6eaef26367af8a18e0cb57978
sithuminiashch <136596859+sithuminiashch@users.noreply.github.com>
2023-08-22 07:18:33 +0530

    Update Q2.scala

--- Q2.scala
+++ Q2.scala
@@ -1,16 +1,11 @@
- import scala.io.StdIn.readInt
-object Q2 {
-  val PatternMatching = (num: Int) => {
-    num match {
-      case x if (x <= 0) => println("Negative/Zero")
-      case x if (x % 2 == 0) => println("Even")
-      case _ => println("Odd")
-    }
-  }
-  def main(args: Array[String]): Unit = {
-    println("Enter Number: ")
-    val num = readInt()
-    PatternMatching(num)
+ object Q2 {
 
+  val PatternMatching: Int => Unit = x => if (x <= 0) println("Negative/Zero")
+  else if (x%2 == 0) println("Even")
+  else if (x%2 == 1) println("Odd")
+
+
+  def main(args: Array[String]): Unit = {
+    PatternMatching(scala.io.StdIn.readInt())
   }
 }
